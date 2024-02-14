67586dc15d6f8fa17928339af7d25dd0332e16c2
lincoln ingaroca <lincolningaroca@gmail.com>
2024-02-14 18:02:16 -0500

    The implementation of the encrypt and decrypt methods is completed.

--- swtablemodel.cpp
+++ swtablemodel.cpp
@@ -1,7 +1,23 @@
 #include "swtablemodel.hpp"
+#include <QSqlRecord>
+#include "util/helper.hpp"
 
-SWTableModel::SWTableModel(QObject *parent)
-  : QSqlTableModel{parent}
+SWTableModel::SWTableModel(QObject *parent, QSqlDatabase db)
+  : QSqlTableModel{parent, db}
 {
+
+}
+
+QVariant SWTableModel::data(const QModelIndex& index, int role) const{
+
+  if(index.column() == 1 && role == Qt::DisplayRole){
+      QSqlRecord record = QSqlTableModel::record(index.row());
+      auto encryptedText = record.value(1).toString();
+
+      auto decryptedData = SW::Helper_t::decrypt(encryptedText);
+      return decryptedData;
+
+    }
+  return QSqlTableModel::data(index, role);
 
 }
--- swtablemodel.hpp
+++ swtablemodel.hpp
@@ -2,10 +2,14 @@
 
 #include <QSqlTableModel>
 
-class SWTableModel : public QSqlTableModel
-{
+class SWTableModel : public QSqlTableModel{
+
   Q_OBJECT
 public:
-  explicit SWTableModel(QObject *parent = nullptr);
+  explicit SWTableModel(QObject *parent = nullptr, QSqlDatabase db = QSqlDatabase());
+
+  // QAbstractItemModel interface
+public:
+  QVariant data(const QModelIndex& index, int role) const override;
 };
 
