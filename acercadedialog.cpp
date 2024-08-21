b2494b6c3cb9f11cbbb5c177b6d6cf7e5fb6300b
Lincoln Ingaroca <lincolningaroca@gmail.com>
2024-08-21 17:42:12 -0500

    Code improvements.

--- acercadedialog.cpp
+++ acercadedialog.cpp
@@ -11,7 +11,7 @@ AcercaDeDialog::AcercaDeDialog(SW::Theme colorMode, QWidget *parent) :
   setTextToAbout();
   ui->tabWidget->setCurrentIndex(0);
 
-  (colorMode == SW::Theme::Dark_Mode) ? ui->lblLogo->setPixmap(QPixmap(":/img/logoEmpresa_1.png").scaled(490, 338)) :
+  (static_cast<bool>(colorMode)) ? ui->lblLogo->setPixmap(QPixmap(":/img/logoEmpresa_1.png").scaled(490, 338)) :
                                                  ui->lblLogo->setPixmap(QPixmap(":/img/logoEmpresa.png").scaled(490, 338));
 
 
