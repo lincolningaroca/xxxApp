782f2cba5dca620194f720481dea0d5e2bd8b751
Lincoln Ingaroca <lincolningaroca@gmail.com>
2025-06-27 23:35:44 -0500

    Code improvements.

--- swtablemodel.cpp
+++ swtablemodel.cpp
@@ -10,17 +10,7 @@ SWTableModel::SWTableModel(QObject *parent, QSqlDatabase db)
 
 QVariant SWTableModel::data(const QModelIndex& index, int role) const{
 
-  // if(index.column() == 1 && role == Qt::DisplayRole){
-  //     QSqlRecord record = QSqlTableModel::record(index.row());
-  //     auto encryptedText = record.value(1).toString();
-
-  //     auto decryptedData = SW::Helper_t::decrypt(encryptedText);
-  //     return decryptedData;
-
-  //   }
   if(role == Qt::DisplayRole && (index.column() == 1 || index.column() == 2) ){
-      // QSqlRecord record = QSqlTableModel::record(index.row());
-      // auto encryptedText = record.value(1).toString();
 
       const auto decryptedData = QSqlTableModel::data(index, role);
       return SW::Helper_t::decrypt(decryptedData.toString());
