e7dc8521fc42cc4adaf43f5798f92255cfaecc50
Lincoln Ingaroca <lincolningaroca@gmail.com>
2025-07-03 14:11:51 -0500

    code improvements.

--- resetpassworddialog.cpp
+++ resetpassworddialog.cpp
@@ -14,137 +14,145 @@ ResetPasswordDialog::ResetPasswordDialog(QWidget *parent)
 
   setInit_Form();
 
+  QObject::connect(ui->stackedWidget, &QStackedWidget::currentChanged, this, [&](int index){
+
+    (index == 0) ? ui->btnAtras->setDisabled(true) : ui->btnAtras->setEnabled(true);
+
+  });
+
   QObject::connect(ui->btnValidarUsuario, &QPushButton::clicked, this, [&](){
 
-      if(ui->txtUser->text() == SW::Helper_t::defaultUser){
-          ui->txtUser->clear();
-          ui->txtUser->setFocus(Qt::OtherFocusReason);
-          return;
-        }
+    if(ui->txtUser->text() == SW::Helper_t::defaultUser || ui->txtUser->text().isEmpty()){
+      ui->txtUser->clear();
+      ui->txtUser->setFocus(Qt::OtherFocusReason);
+      return;
+    }
 
-      if(!helper.userExists(ui->txtUser->text().simplified())){
-          QMessageBox::warning(this, SW::Helper_t::appName(),
-                               QStringLiteral("<p><cite>Nombre de usuario incorrecto.</cite></p>"));
-          ui->txtUser->selectAll();
-          ui->txtUser->setFocus(Qt::OtherFocusReason);
-          return;
-        }
-      const auto user = SW::Helper_t::hashGenerator(ui->txtUser->text().simplified().toLatin1());
-      userId_ = helper.getUser_id(user, SW::User::U_user);
-      auto rescue_type = helper.validateRescueType(userId_);
+    if(!helper.userExists(ui->txtUser->text().simplified())){
+      QMessageBox::warning(this, SW::Helper_t::appName(),
+                           QStringLiteral("<p><cite>Nombre de usuario incorrecto.</cite></p>"));
+      ui->txtUser->selectAll();
+      ui->txtUser->setFocus(Qt::OtherFocusReason);
+      return;
+    }
+    const auto user = SW::Helper_t::hashGenerator(ui->txtUser->text().simplified().toLatin1());
+    userId_ = helper.getUser_id(user, SW::User::U_user);
+    auto rescue_type = helper.validateRescueType(userId_);
 
-      if(rescue_type == LogInDialog::authType.value(SW::AuthType::Secret_Question)){
-          ui->stackedWidget->setCurrentIndex(1);
-          ui->txtPregunta->setPlainText(helper.getQuestion(userId_));
-          ui->btnRespuesta->setDefault(true);
-          ui->txtRespuesta->setFocus(Qt::OtherFocusReason);
+    if(rescue_type == LogInDialog::authType.value(SW::AuthType::Secret_Question)){
+      ui->stackedWidget->setCurrentIndex(1);
+      ui->txtPregunta->setPlainText(helper.getQuestion(userId_));
+      ui->btnRespuesta->setDefault(true);
+      ui->txtRespuesta->setFocus(Qt::OtherFocusReason);
 
-        }
-      else{
-          ui->stackedWidget->setCurrentIndex(2);
-          ui->btnClaveNumerica->setDefault(true);
-          ui->txtPIN->setFocus(Qt::OtherFocusReason);
-        }
+    }
+    else{
+      ui->stackedWidget->setCurrentIndex(2);
+      ui->btnClaveNumerica->setDefault(true);
+      ui->txtPIN->setFocus(Qt::OtherFocusReason);
+    }
 
 
-    });
+  });
 
   //btn pregunta secreta
   QObject::connect(ui->btnRespuesta, &QAbstractButton::clicked, this, [&](){
-      if(!helper.validateAnswer(ui->txtRespuesta->text(), userId_)){
-          QMessageBox::warning(this, SW::Helper_t::appName(),
-                               QStringLiteral("<p><cite>Su respuesta es incorrecta.</cite></p>"));
-          ui->txtRespuesta->selectAll();
-          ui->txtRespuesta->setFocus(Qt::OtherFocusReason);
-          return;
-        }
+    if(!helper.validateAnswer(ui->txtRespuesta->text(), userId_)){
+      QMessageBox::warning(this, SW::Helper_t::appName(),
+                           QStringLiteral("<p><cite>Su respuesta es incorrecta.</cite></p>"));
+      ui->txtRespuesta->selectAll();
+      ui->txtRespuesta->setFocus(Qt::OtherFocusReason);
+      return;
+    }
 
-      ui->stackedWidget->setCurrentIndex(3);
-      ui->txtNewPassword->setFocus(Qt::OtherFocusReason);
-      ui->btnReset->setDefault(true);
-    });
+    ui->stackedWidget->setCurrentIndex(3);
+    ui->txtNewPassword->setFocus(Qt::OtherFocusReason);
+    ui->btnReset->setDefault(true);
+  });
 
   //btn pin numerico
   QObject::connect(ui->btnClaveNumerica, &QPushButton::clicked, this, [&](){
 
-      if(ui->txtPIN->text().size() < 4){
-          QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><em>El PIN numérico debe contener 4 digitos!</em></span>"));
-          ui->txtPIN->selectAll();
-          ui->txtPIN->setFocus(Qt::OtherFocusReason);
-          return;
-        }
-      if(!helper.validateAnswer(ui->txtPIN->text(), userId_)){
-          QMessageBox::warning(this, SW::Helper_t::appName(),
-                               QStringLiteral("<p><em>El número que ingreso es incorrecto.</em></p>"));
-          ui->txtPIN->selectAll();
-          ui->txtPIN->setFocus(Qt::OtherFocusReason);
-          return;
-        }
+    if(ui->txtPIN->text().size() < 4){
+      QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><em>El PIN numérico debe contener 4 digitos!</em></span>"));
+      ui->txtPIN->selectAll();
+      ui->txtPIN->setFocus(Qt::OtherFocusReason);
+      return;
+    }
+    if(!helper.validateAnswer(ui->txtPIN->text(), userId_)){
+      QMessageBox::warning(this, SW::Helper_t::appName(),
+                           QStringLiteral("<p><em>El número que ingreso es incorrecto.</em></p>"));
+      ui->txtPIN->selectAll();
+      ui->txtPIN->setFocus(Qt::OtherFocusReason);
+      return;
+    }
 
-      ui->stackedWidget->setCurrentIndex(3);
-      ui->txtNewPassword->setFocus(Qt::OtherFocusReason);
-      ui->btnReset->setDefault(true);
-    });
+    ui->stackedWidget->setCurrentIndex(3);
+    ui->txtNewPassword->setFocus(Qt::OtherFocusReason);
+    ui->btnReset->setDefault(true);
+  });
 
   //btn volver atras
 
   QObject::connect(ui->btnAtras, &QPushButton::clicked, this, [&](){
 
-      ui->txtUser->clear();
-      ui->txtNewPassword->clear();
-      ui->txtPIN->clear();
-      ui->txtPregunta->clear();
-      ui->txtRePassword->clear();
-      ui->txtRespuesta->clear();
+    ui->txtUser->clear();
+    ui->txtNewPassword->clear();
+    ui->txtPIN->clear();
+    ui->txtPregunta->clear();
+    ui->txtRePassword->clear();
+    ui->txtRespuesta->clear();
 
-      ui->stackedWidget->setCurrentIndex(0);
-      ui->btnValidarUsuario->setDefault(true);
-      ui->txtUser->setFocus(Qt::OtherFocusReason);
-    });
+    ui->stackedWidget->setCurrentIndex(0);
+    ui->btnValidarUsuario->setDefault(true);
+    ui->txtUser->setFocus(Qt::OtherFocusReason);
+  });
+
+
 
   //btn reset password
   QObject::connect(ui->btnReset, &QPushButton::clicked, this, [&](){
-      if(ui->txtNewPassword->text().isEmpty()){
-          QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><em>Este campo es requerido.</em></span>"));
-          ui->txtNewPassword->setFocus(Qt::OtherFocusReason);
-          return;
-        }
-      if(ui->txtRePassword->text().isEmpty()){
-          QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><em>Este campo es requerido.</em></span>"));
-          ui->txtRePassword->setFocus(Qt::OtherFocusReason);
-          return;
-        }
-      if(ui->txtRePassword->text().simplified() != ui->txtNewPassword->text().simplified()){
-          QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><strong><em>La clave o password de confirmación no coincide.</em></strong></span>"));
-          ui->txtRePassword->selectAll();
-          ui->txtRePassword->setFocus(Qt::OtherFocusReason);
-          return;
-        }
-      if(ui->txtNewPassword->text().size() < 8 || ui->txtRePassword->text().size() < 8){
-          QMessageBox::warning(this, SW::Helper_t::appName(),
-                               QStringLiteral("<span>"
-                                              "<em>"
-                                              "El password o clave, debe tener 8 caracteres como mínimo."
-                                              "</em>"
-                                              "</span>"));
-          ui->txtRePassword->selectAll();
-          ui->txtRePassword->setFocus();
-          return;
-        }
-      if(!SW::Helper_t::isPasswordSecure(ui->txtRePassword->text().simplified())){
-          QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral(
-                                 "<span><strong><em>La clave o password no es seguro.<br>"
-                                 "Debe contener al menos una Letra Mayuscula, una miniscula, numeros y caracteres especiales.</em></strong></span>"));
-          ui->txtRePassword->selectAll();
-          ui->txtRePassword->setFocus();
-          return;
-        }
-      if(helper.resetPassword(ui->txtRePassword->text(), userId_)){
-          QMessageBox::information(this, SW::Helper_t::appName(), QStringLiteral("<span><em>Su clave o password de acceso fue cambiado con éxito!</em></strong></span>"));
-          //          ui->btnAtras->click();
-          accept();
-        }
-    });
+    if(ui->txtNewPassword->text().isEmpty()){
+      QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><em>Este campo es requerido.</em></span>"));
+      ui->txtNewPassword->setFocus(Qt::OtherFocusReason);
+      return;
+    }
+    if(ui->txtRePassword->text().isEmpty()){
+      QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><em>Este campo es requerido.</em></span>"));
+      ui->txtRePassword->setFocus(Qt::OtherFocusReason);
+      return;
+    }
+    if(ui->txtRePassword->text().simplified() != ui->txtNewPassword->text().simplified()){
+      QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral("<span><strong><em>La clave o password de confirmación no coincide.</em></strong></span>"));
+      ui->txtRePassword->selectAll();
+      ui->txtRePassword->setFocus(Qt::OtherFocusReason);
+      return;
+    }
+    if(ui->txtNewPassword->text().size() < 8 || ui->txtRePassword->text().size() < 8){
+      QMessageBox::warning(this, SW::Helper_t::appName(),
+                           QStringLiteral("<span>"
+                                          "<em>"
+                                          "El password o clave, debe tener 8 caracteres como mínimo."
+                                          "</em>"
+                                          "</span>"));
+      ui->txtRePassword->selectAll();
+      ui->txtRePassword->setFocus();
+      return;
+    }
+    if(!SW::Helper_t::isPasswordSecure(ui->txtRePassword->text().simplified())){
+      QMessageBox::warning(this, SW::Helper_t::appName(), QStringLiteral(
+                                                            "<span><strong><em>La clave o password no es seguro.<br>"
+                                                            "Debe contener al menos una Letra Mayuscula, una miniscula, numeros y caracteres especiales.</em></strong></span>"));
+      ui->txtRePassword->selectAll();
+      ui->txtRePassword->setFocus();
+      return;
+    }
+    if(helper.resetPassword(ui->txtRePassword->text(), userId_)){
+      QMessageBox::information(this, SW::Helper_t::appName(), QStringLiteral("<span><em>Su clave o password de acceso fue cambiado con éxito!</em></strong></span>"));
+      //          ui->btnAtras->click();
+      accept();
+    }
+  });
 }// fin del constructor
 
 ResetPasswordDialog::~ResetPasswordDialog(){
@@ -154,16 +162,16 @@ ResetPasswordDialog::~ResetPasswordDialog(){
 void ResetPasswordDialog::setFeatures(QLineEdit *w, QCheckBox *b, bool checked) noexcept{
 
   if(checked){
-      w->setEchoMode(QLineEdit::Normal);
-      b->setIcon(QIcon(QStringLiteral(":/img/open.png")));
-      b->setToolTip("Ocultar los caracteres.");
-    }
+    w->setEchoMode(QLineEdit::Normal);
+    b->setIcon(QIcon(QStringLiteral(":/img/open.png")));
+    b->setToolTip("Ocultar los caracteres.");
+  }
   else{
-      w->setEchoMode(QLineEdit::Password);
-      b->setIcon(QIcon(QStringLiteral(":/img/close.png")));
-      b->setToolTip("Mostrar los caracteres.");
+    w->setEchoMode(QLineEdit::Password);
+    b->setIcon(QIcon(QStringLiteral(":/img/close.png")));
+    b->setToolTip("Mostrar los caracteres.");
 
-    }
+  }
 
 }
 
@@ -173,6 +181,8 @@ void ResetPasswordDialog::setInit_Form() noexcept{
   ui->stackedWidget->setCurrentIndex(0);
   ui->txtUser->setClearButtonEnabled(true);
 
+  if(ui->stackedWidget->currentIndex() == 0)   ui->btnAtras->setDisabled(true);
+
   auto* validator = new QRegularExpressionValidator(QRegularExpression(QStringLiteral("^\\d{4}$")), this);
   ui->txtPIN->setValidator(validator);
 
@@ -181,20 +191,21 @@ void ResetPasswordDialog::setInit_Form() noexcept{
   ui->txtRePassword->setEchoMode(QLineEdit::Password);
   ui->txtRespuesta->setEchoMode(QLineEdit::Password);
 
+
   //iconos y acciones para QLineEdit para mostrar y/o ocultar los caracteres de los controles.
   QObject::connect(ui->checkBox, &QCheckBox::clicked, this, [&](bool checked){
-      setFeatures(ui->txtRespuesta, ui->checkBox, checked);
+    setFeatures(ui->txtRespuesta, ui->checkBox, checked);
 
 
-    });
+  });
   QObject::connect(ui->checkBox_2, &QCheckBox::clicked, this, [&](bool checked){
-      setFeatures(ui->txtPIN, ui->checkBox_2, checked);
-    });
+    setFeatures(ui->txtPIN, ui->checkBox_2, checked);
+  });
   QObject::connect(ui->checkBox_3, &QCheckBox::clicked, this, [&](bool checked){
-      setFeatures(ui->txtNewPassword, ui->checkBox_3, checked);
-    });
+    setFeatures(ui->txtNewPassword, ui->checkBox_3, checked);
+  });
   QObject::connect(ui->checkBox_4, &QCheckBox::clicked, this, [&](bool checked){
-      setFeatures(ui->txtRePassword, ui->checkBox_4, checked);
-    });
+    setFeatures(ui->txtRePassword, ui->checkBox_4, checked);
+  });
 
 }
